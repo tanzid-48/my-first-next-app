1aab4af64c695cc6fc090a720851cd46ce5ace4b
tanzid-48 <mdtanzid.525@gmail.com>
2026-04-09 22:13:21 +0600

    Image Optimization in Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,20 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+   
+  // https://images.unsplash.com/photo-1775716021167-e8133881470a
+
+   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/photo-1775716021167-e8133881470a/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,33 @@
+import Image from 'next/image';
+import Me from '../../assets/iamges/_MG_0228.png'
 import React from 'react';
 
 const AboutPage = () => {
     return (
         <div>
          <h2>About Pages</h2>   
+        <div className="flex justify-center">
+      <Image
+         src= "/23.jpg"
+         alt='Md tanzid'
+         width={300}
+         height={200}
+         >
+      </Image>
+    <Image
+      src="https://images.unsplash.com/photo-1775716021167-e8133881470a"
+      alt="Picture of the author"
+      width={300}
+      height={200}
+    />
+  </div>
+
+      <Image
+      src={Me}
+      alt=''
+      width={400}
+      height={300}
+      ></Image>
         </div>
     );
 };
